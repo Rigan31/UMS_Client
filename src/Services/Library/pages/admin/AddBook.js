c7c1722c497ebb07a179a0929208d33f0592c904
Piyal <souvik.piyal@gmail.com>
2022-08-14 03:37:04 +0600

    feedback done

--- src/Services/Library/pages/admin/AddBook.js
+++ src/Services/Library/pages/admin/AddBook.js
@@ -28,23 +28,21 @@ export default function AddBook() {
     }
 
     return (    
-        <div>
+        <div className="def">
             <SidebarLibraryAdmin/>
-            <div class="jumbotron text-center" id="jumbotron">
+            <div class="jumbotron text-center box" id="jumbotron">
                 <h1 class="display-3">Add Book</h1>
                 <p class="lead">Book Description</p>
                 <hr class="my-y-2"/>
                 <p>Please give the following info</p>
-                <form class="form-inline justify-content-center">
-
-                    <div class="input-group mb-2 mr-sm-2">
-                        <div class="input-group-prepend">
-                            <input type="name" class="form-control" id="name" placeholder="Book Name" onChange={e => setBookName(e.target.value)}/><br/>
-                            <input type="name" class="form-control" id="author" placeholder="Author"onChange={e => setAuthorName(e.target.value)}/><br/>
-                            <input type="name" class="form-control" id="quantity" placeholder="Quantity"onChange={e => setQuantity(e.target.value)}/><br/>
-                        </div>
-                    </div>
-
+                <form class="justify-content-center">
+                    <label>Book Name: </label>
+                    <input type="name" class="form-control" id="name" placeholder="Book Name" onChange={e => setBookName(e.target.value)}/><br/>
+                    <label>Author Name: </label>
+                    <input type="name" class="form-control" id="author" placeholder="Author"onChange={e => setAuthorName(e.target.value)}/><br/>
+                    <label>Quantity: </label>
+                    <input type="name" class="form-control" id="quantity" placeholder="Quantity"onChange={e => setQuantity(e.target.value)}/><br/>
+                     
                     <button type="submit" class="btn btn-primary mb-2" onClick={ e => save_book(e) }>Add</button>
                 </form>
             </div>
